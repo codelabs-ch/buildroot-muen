9b83d1faaa895dffa5b8a4cfb8b288466bf29fdc
Reto Buerki <reet@codelabs.ch>
2015-04-16 09:08:22 +0200

    Add custom lmods package
    The package prepares the custom Linux kernel headers for external module
    builds.

---
+++ package/muen/lmods/lmods.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# lmods
+#
+################################################################################
+
+LMODS_SOURCE  =
+LMODS_VERSION = 0.1
+
+LMODS_DEPENDENCIES = linux-headers
+
+LMODS_KNL_VER = $(call qstrip,$(BR2_DEFAULT_KERNEL_HEADERS))
+LMODS_KNL_SRC = $(TOPDIR)/output/build/linux-headers-$(LMODS_KNL_VER)
+
+LMODS_KNL_CNF_PRJ   = http://git.codelabs.ch/?p=muen.git
+LMODS_KNL_CNF_OPTS  = hb=refs/heads/devel;a=blob_plain
+LMODS_KNL_CNF_FILE  = f=components/linux/config/linux64-3.18
+LMODS_KERNEL_CONFIG = \
+	$(LMODS_KNL_CNF_PRJ);$(LMODS_KNL_CNF_OPTS);$(LMODS_KNL_CNF_FILE)
+
+LMODS_MAKE_OPTS  = DESTDIR=$(TARGET_DIR)
+LMODS_MAKE_OPTS += KERNEL_SOURCE=$(LMODS_KNL_SRC)
+
+define LMODS_BUILD_CMDS
+	wget -c "$(LMODS_KERNEL_CONFIG)" -O "$(LMODS_KNL_SRC)/.config"
+	$(MAKE) CC="$(TARGET_CC)" -C $(LMODS_KNL_SRC) modules_prepare
+endef
+
+define LMODS_CLEAN_CMDS
+	$(MAKE) linux-headers-dirclean
+	$(MAKE) linux-headers-install-staging
+endef
+
+$(eval $(generic-package))

b761a1c0810ab1066896dd881588549bf3a6dbef
Reto Buerki <reet@codelabs.ch>
2015-05-19 09:44:00 +0200

    Add custom muennet package
    The muennet Linux kernel module implements a virtual network interface
    driver which sends and receives data via shared memory channels provided
    by the Muen Separation Kernel.

---
+++ package/muen/muennet/muennet.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# muennet
+#
+################################################################################
+
+MUENNET_VERSION     = master
+MUENNET_SITE        = http://git.codelabs.ch/git/muen/linux/muennet.git
+MUENNET_SITE_METHOD = git
+
+MUENNET_DEPENDENCIES = lmods
+
+MUENNET_KNL_VER = $(call qstrip,$(BR2_DEFAULT_KERNEL_HEADERS))
+MUENNET_KNL_SRC = $(TOPDIR)/output/build/linux-headers-$(MUENNET_KNL_VER)
+
+MUENNET_MAKE_OPTS  = DESTDIR=$(TARGET_DIR)
+MUENNET_MAKE_OPTS += KERNEL_SOURCE=$(MUENNET_KNL_SRC)
+
+define MUENNET_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" $(MUENNET_MAKE_OPTS) -C $(@D)
+endef
+
+define MUENNET_INSTALL_TARGET_CMDS
+	$(MAKE) CC="$(TARGET_CC)" $(MUENNET_MAKE_OPTS) -C $(@D) install
+endef
+
+define MUENNET_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/lib/modules/extra/muennet.ko
+endef
+
+$(eval $(generic-package))

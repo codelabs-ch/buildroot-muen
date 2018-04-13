3035505a9c14252d84028b3af1ab06559a9f20f1
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2018-04-13 12:20:46 +0200

    ioquake3: new package
    Signed-off-by: Adrian-Ken Rueegsegger <ken@codelabs.ch>

---
+++ package/ioquake3/ioquake3.mk
@@ -0,0 +1,54 @@
+################################################################################
+#
+# ioquake3
+#
+################################################################################
+
+IOQUAKE3_VERSION       = master
+IOQUAKE3_SITE          = https://github.com/ioquake/ioq3.git
+IOQUAKE3_SITE_METHOD   = git
+IOQUAKE3_LICENSE       = GPL-2.0
+IOQUAKE3_LICENSE_FILES = COPYING
+IOQUAKE3_DEPENDENCIES  = host-pkgconf jpeg libgl libglu libpng openal sdl2 zlib
+
+IOQUAKE3_MAKE_OPTS = USE_INTERNAL_LIBS=0 \
+					 COPYDIR=$(TARGET_DIR)/usr/games/quake3 \
+					 PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig
+
+ifeq ($(BR2_PACKAGE_LIBCURL),y)
+IOQUAKE3_DEPENDENCIES += libcurl
+IOQUAKE3_MAKE_OPTS += USE_CURL=1
+else
+IOQUAKE3_MAKE_OPTS += USE_CURL=0
+endif
+
+ifeq ($(BR2_PACKAGE_FREETYPE),y)
+IOQUAKE3_DEPENDENCIES += freetype
+IOQUAKE3_MAKE_OPTS += USE_FREETYPE=1
+else
+IOQUAKE3_MAKE_OPTS += USE_FREETYPE=0
+endif
+
+ifeq ($(BR2_PACKAGE_OPUSFILE),y)
+IOQUAKE3_DEPENDENCIES += opusfile
+IOQUAKE3_MAKE_OPTS += USE_CODEC_OPUS=1
+else
+IOQUAKE3_MAKE_OPTS += USE_CODEC_OPUS=0
+endif
+
+ifeq ($(BR2_PACKAGE_LIBVORBIS),y)
+IOQUAKE3_DEPENDENCIES += libvorbis
+IOQUAKE3_MAKE_OPTS += USE_CODEC_VORBIS=1
+else
+IOQUAKE3_MAKE_OPTS += USE_CODEC_VORBIS=0
+endif
+
+define IOQUAKE3_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" $(IOQUAKE3_MAKE_OPTS) -C $(@D)
+endef
+
+define IOQUAKE3_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" $(IOQUAKE3_MAKE_OPTS) -C $(@D) copyfiles
+endef
+
+$(eval $(generic-package))

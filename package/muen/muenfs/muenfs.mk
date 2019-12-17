90db1f9fc295872aaa4e5b02e3a94abfeced974c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-12-17 18:01:21 +0100

    Drop Muen Linux modules and lmods
    The Muen out-of-tree kernel modules are now built and packaged as a
    separate initramfs and no longer need to be part of buildroot.
    
    Note: the muenfs package now only consists of the test binaries.

--- package/muen/muenfs/muenfs.mk
+++ package/muen/muenfs/muenfs.mk
@@ -4,28 +4,21 @@
 #
 ################################################################################
 
-MUENFS_VERSION     = 12f31e82a08f105163ef4e2ada286e8466aab344
+MUENFS_VERSION     = d5a5889e3a98a3a3752c72883aa89ee70353225c
 MUENFS_SITE        = https://git.codelabs.ch/git/muen/linux/muenfs.git
 MUENFS_SITE_METHOD = git
 
-MUENFS_DEPENDENCIES = lmods
-
-MUENFS_KNL_VER = $(call qstrip,$(BR2_DEFAULT_KERNEL_HEADERS))
-MUENFS_KNL_SRC = $(TOPDIR)/output/build/linux-headers-$(MUENFS_KNL_VER)
-
-MUENFS_MAKE_OPTS  = DESTDIR=$(TARGET_DIR)
-MUENFS_MAKE_OPTS += KERNEL_SOURCE=$(MUENFS_KNL_SRC)
+MUENFS_MAKE_OPTS = DESTDIR=$(TARGET_DIR)
 
 define MUENFS_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" $(MUENFS_MAKE_OPTS) -C $(@D)
+	$(MAKE) CC="$(TARGET_CC)" $(MUENFS_MAKE_OPTS) -C $(@D) compile-test
 endef
 
 define MUENFS_INSTALL_TARGET_CMDS
-	$(MAKE) CC="$(TARGET_CC)" $(MUENFS_MAKE_OPTS) -C $(@D) install
+	$(MAKE) CC="$(TARGET_CC)" $(MUENFS_MAKE_OPTS) -C $(@D) install-test
 endef
 
 define MUENFS_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/lib/modules/extra/muenfs.ko
 	rm -f $(TARGET_DIR)/usr/sbin/muenfs-test
 	rm -f $(TARGET_DIR)/usr/sbin/muenfs-marker
 endef

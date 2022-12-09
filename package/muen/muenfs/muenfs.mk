31a70a1eb54634585a67a2d2474c671cd67edd54
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2022-12-09 19:16:34 +0100

    Update muenfs revision
    Includes new muenfs-example test program.

--- package/muen/muenfs/muenfs.mk
+++ package/muen/muenfs/muenfs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MUENFS_VERSION     = f24a9deba6a76ab343f1bc3d88cb7ae272864a95
+MUENFS_VERSION     = a554aa0109d8c372e67f4b4d0a38fdcc5610743d
 MUENFS_SITE        = https://git.codelabs.ch/git/muen/linux/muenfs.git
 MUENFS_SITE_METHOD = git
 
@@ -21,6 +21,7 @@ endef
 define MUENFS_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/sbin/muenfs-test
 	rm -f $(TARGET_DIR)/usr/sbin/muenfs-marker
+	rm -f $(TARGET_DIR)/usr/sbin/muenfs-example
 endef
 
 $(eval $(generic-package))
